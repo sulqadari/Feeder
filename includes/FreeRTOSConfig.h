df70d841eff0219a069cd08cc8c6e1f6fe22fc04
Telman Islamov <sulqadari@gmail.com>
2025-01-27 23:51:04 +0300

    Minor changes in FreeRTOSConfig.h file

--- includes/FreeRTOSConfig.h
+++ includes/FreeRTOSConfig.h
@@ -91,27 +91,28 @@
 #define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )	
 #define configSYSTICK_CLOCK_HZ 		( configCPU_CLOCK_HZ / 8 )
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES		( 3 )
+#define configMAX_PRIORITIES		( 5 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 8 * 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 16 )
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
-#define configIDLE_SHOULD_YIELD		1
+#define configIDLE_SHOULD_YIELD		0
 #define configUSE_MUTEXES			1
+#define configUSE_TASK_NOTIFICATIONS	1
+#define configUSE_TIME_SLICING			1
+#define configUSE_RECURSIVE_MUTEXES		0
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 			0
 #define configMAX_CO_ROUTINE_PRIORITIES	( 2 )
 
-#define configSUPPORT_STATIC_ALLOCATION		0
-#define configSUPPORT_DYNAMIC_ALLOCATION	1
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
 #define INCLUDE_vTaskPrioritySet		1
 #define INCLUDE_uxTaskPriorityGet		1
-#define INCLUDE_vTaskDelete				1
+#define INCLUDE_vTaskDelete				0
 #define INCLUDE_vTaskCleanUpResources	0
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
