9d2c456e91edcc7767910d7d4f306dd71e8fbc14
Telman Islamov <t.islamov@aladdin.ru>
2025-04-08 17:50:09 +0300

    implemented FLASH_write() function

--- hal/hal_flash.c
+++ hal/hal_flash.c
@@ -1,5 +1,11 @@
 #include "hal_flash.h"
 
+void
+FLASH_Init(void)
+{
+
+}
+
 Flash_SW
 FLASH_readWord(uint32_t addr, uint32_t* word)
 {
@@ -21,4 +27,53 @@ FLASH_readByte(uint32_t addr, uint8_t* byte)
 {
     *byte = *(uint8_t*)addr;
     return fsw_OK;
+}
+
+static Flash_SW
+FLASH_pageErase(uint32_t pageAddr)
+{
+    uint32_t aligned = (pageAddr + 3) & 0xFFFFFFC;
+    while (FLASH->SR & FLASH_SR_BSY) { ; }  // Wait until current (if any) memory operation being commited.
+
+    FLASH->CR |= FLASH_CR_PER;
+    FLASH->AR |= aligned;   // select a page to erase
+    FLASH->CR |= FLASH_CR_STRT;
+
+    while (FLASH->SR & FLASH_SR_BSY) { ; }  // Wait until current (if any) memory operation being commited.
+}
+
+static Flash_SW
+FLASH_unlock(void)
+{
+    FLASH->KEYR = 0x45670123;
+    FLASH->KEYR = 0xCDEF89AB;
+    return fsw_OK;
+}
+
+static Flash_SW
+FLASH_lock(void)
+{
+    FLASH->CR |= FLASH_CR_LOCK;
+    return fsw_OK;
+}
+
+Flash_SW
+FLASH_write(uint32_t addr, uint16_t data)
+{
+    uint32_t aligned = (addr + 2) & 0xFFFFFFE;
+    uint16_t temp = 0;
+    FLASH_unlock();
+
+    while (FLASH->SR & FLASH_SR_BSY) { ; }  // Wait until current (if any) memory operation being commited.
+    FLASH->CR |= FLASH_CR_PG;
+
+    FLASH_readHalf(aligned, &temp);
+    if (temp != 0xFFFF) {
+        FLASH_pageErase(addr);
+    }
+
+    *(uint16_t*)aligned = data;
+    while (FLASH->SR & FLASH_SR_BSY) { ; }  // Wait until current (if any) memory operation being commited.
+
+    FLASH_lock();
 }
--- hal/hal_flash.h
+++ hal/hal_flash.h
@@ -13,4 +13,6 @@ Flash_SW FLASH_readWord(uint32_t addr, uint32_t* word);
 Flash_SW FLASH_readHalf(uint32_t addr, uint16_t* half);
 Flash_SW FLASH_readByte(uint32_t addr, uint8_t* byte);
 
+Flash_SW FLASH_write(uint32_t addr, uint16_t data);
+
 #endif /* FLASH_HAL_H */
