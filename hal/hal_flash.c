7fdebc552ab350a289d468136a15f7a6079a7c20
Telman Islamov <t.islamov@aladdin.ru>
2025-04-09 18:36:38 +0300

    addressed the compile-time warnings

--- hal/hal_flash.c
+++ hal/hal_flash.c
@@ -10,7 +10,7 @@ Flash_SW
 FLASH_readWord(uint32_t addr, uint32_t* word)
 {
     uint32_t aligned = (addr + 3) & 0xFFFFFFC;
-    *word = (uint32_t*)aligned;
+    *word = *(uint32_t*)aligned;
     return fsw_OK;
 }
 
@@ -30,26 +30,27 @@ FLASH_readByte(uint32_t addr, uint8_t* byte)
 }
 
 static Flash_SW
-FLASH_pageErase(uint32_t pageAddr)
+erasePage(uint32_t pageAddr)
 {
     uint32_t aligned = (pageAddr + 3) & 0xFFFFFFC;
+
     while (FLASH->SR & FLASH_SR_BSY) { ; }  // Wait until current (if any) memory operation being commited.
     if (FLASH->SR & FLASH_SR_EOP) {
         FLASH->SR &= FLASH_SR_EOP;
     }
 
-    FLASH->CR |= FLASH_CR_PER;
-    FLASH->AR |= aligned;   // select a page to erase
-    FLASH->CR |= FLASH_CR_STRT;
+    FLASH->CR |= FLASH_CR_PER;  // Page erase chosen
+    FLASH->AR |= aligned;       // select a page to erase
+    FLASH->CR |= FLASH_CR_STRT; // Trigger the start of erase operation
 
     while (FLASH->SR & FLASH_SR_EOP) { ; }  // Wait until current (if any) memory operation being commited.
-    
     FLASH->SR &= FLASH_SR_EOP;
-    FLASH->CR &= ~FLASH_CR_PER;
+    FLASH->CR &= ~FLASH_CR_PER;  // Reset the 'Page erase chosen' bit
+    return fsw_OK;
 }
 
 static Flash_SW
-FLASH_unlock(void)
+unlockFlash(void)
 {
     FLASH->KEYR = 0x45670123;
     FLASH->KEYR = 0xCDEF89AB;
@@ -57,7 +58,7 @@ FLASH_unlock(void)
 }
 
 static Flash_SW
-FLASH_lock(void)
+lockFlash(void)
 {
     FLASH->CR |= FLASH_CR_LOCK;
     return fsw_OK;
@@ -66,29 +67,29 @@ FLASH_lock(void)
 static Flash_SW
 tryToWrite(uint32_t addr, uint32_t aligned, uint16_t data)
 {
-    FLASH->CR |= FLASH_CR_PG;
-    while (1) {
+    FLASH->CR |= FLASH_CR_PG;               // Set the 'Programming' flag
+    do {
         *(uint16_t*)aligned = data;         // Assign a half-word
 
         if (FLASH->SR & FLASH_SR_PGERR) {   // Does assignment led to error because of non-FF state of the memory?
-            FLASH_pageErase(addr);          // clear the page
+            erasePage(addr);                // clear the page
             FLASH->SR &= FLASH_SR_PGERR;    // Reset the PGERR flag
             continue;                       // Try to write again
         }
 
-        while (!(FLASH->SR & FLASH_SR_EOP)) { ; }  // Wait until current (if any) memory operation being commited.
-        FLASH->SR &= FLASH_SR_EOP;
-        FLASH->CR |= FLASH_CR_PG;
-        break;
-    }
+        while (!(FLASH->SR & FLASH_SR_EOP)) { ; }   // Wait until current (if any) memory operation being commited.
+        FLASH->SR &= FLASH_SR_EOP;                  // Reset the 'End Of Programming' flag
+        FLASH->CR &= ~FLASH_CR_PG;                  // Reset the 'Programming' flag
+    } while (0);
+    return fsw_OK;
 }
 
 Flash_SW
 FLASH_write(uint32_t addr, uint16_t data)
 {
     uint32_t aligned = (addr + 2) & 0xFFFFFFE;
-    uint16_t temp = 0;
-    FLASH_unlock();
+
+    unlockFlash();
 
     while (FLASH->SR & FLASH_SR_BSY) { ; }
     if (FLASH->SR & FLASH_SR_EOP) {
@@ -97,5 +98,6 @@ FLASH_write(uint32_t addr, uint16_t data)
 
     tryToWrite(addr, aligned, data);
 
-    FLASH_lock();
+    lockFlash();
+    return fsw_OK;
 }
